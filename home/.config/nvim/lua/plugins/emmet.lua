1a255251d17799e061187a6a47be2d2d8498d5f7
Jaye Heffernan <jpeheffernan@gmail.com>
2023-07-06 19:38:20 +1000

    Remove Emmet LSP
    Can't get it working properly

--- home/.config/nvim/lua/plugins/emmet.lua
+++
@@ -1,5 +0,0 @@
-return {
-  {
-    "olrtg/emmet-language-server",
-  },
-}
--- home/.config/nvim/lua/plugins/nvim-lsp.lua
+++ home/.config/nvim/lua/plugins/nvim-lsp.lua
@@ -1,56 +1,3 @@
-vim.lsp.set_log_level("debug")
-local lspconfig = require("lspconfig")
-local util = require("lspconfig.util")
-local configs = require("lspconfig.configs")
-
--- Check if it's already defined for when reloading this file.
--- print(vim.inspect(configs))
-if not configs.emmet_language_server then
-  configs.emmet_language_server = {
-    -- capabilities = vim.lsp.protocol.make_client_capabilities(),
-    default_config = {
-      cmd = { "emmet-language-server", "--stdio" },
-      root_dir = util.find_git_ancestor,
-      -- autostart = true,
-      -- single_file_support = true,
-      -- name = "emmet_language_server",
-      filetypes = {
-        "astro",
-        "css",
-        "eruby",
-        "html",
-        "htmldjango",
-        "javascript",
-        "javascriptreact",
-        "less",
-        "pug",
-        "sass",
-        "scss",
-        "svelte",
-        "typescript",
-        "typescriptreact",
-        "vue",
-      },
-      init_options = {
-        --- @type table<string, any> https://docs.emmet.io/customization/preferences/
-        preferences = {},
-        --- @type "always" | "never" Defaults to `"always"`
-        showExpandedAbbreviation = "always",
-        --- @type boolean Defaults to `true`
-        showAbbreviationSuggestions = true,
-        --- @type boolean Defaults to `false`
-        showSuggestionsAsSnippets = true,
-        --- @type table<string, any> https://docs.emmet.io/customization/syntax-profiles/
-        syntaxProfiles = {},
-        --- @type table<string, string> https://docs.emmet.io/customization/snippets/#variables
-        variables = {},
-        --- @type string[]
-        excludeLanguages = {},
-      },
-    },
-  }
-end
-
 return {
   {
     "neovim/nvim-lspconfig",
@@ -76,8 +23,6 @@ return {
             definitionProvider = false,
           },
         },
-        -- NB: may npm i -g @olrtg/emmet-language-server
-        emmet_language_server = {},
       },
     },
   },
