1cc9fcd14851819eca2622b467f4c98eb0d28ac3
Jaye Heffernan <jpeheffernan@gmail.com>
2023-07-06 17:49:37 +1000

    Fix completion menu

--- home/.config/nvim/lua/plugins/dadbod.lua
+++ home/.config/nvim/lua/plugins/dadbod.lua
@@ -1,12 +1,19 @@
 return {
-  "tpope/vim-dadbod",
-  dependencies = {
-    {
-      "kristijanhusak/vim-dadbod-completion",
+  {
+    "tpope/vim-dadbod",
+    dependencies = {
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        -- Completion must be loaded first, then dadbod completion can add to
+        -- it, otherwise vim-dadbod-completion messes up normal completion
+        -- somehow
+        dependencies = { { "hrsh7th/nvim-cmp" } },
+      },
     },
+    init = function()
+      -- todo Redis DB
+      vim.g.db = "postgresql://postgres:@localhost:5432/ludwig"
+      vim.g.omni_sql_no_default_maps = 1 -- https://www.reddit.com/r/vim/comments/2om1ib/how_to_disable_sql_dynamic_completion/cmop4zh/
+    end,
   },
-  init = function()
-    vim.g.db = "postgresql://postgres:@localhost:5432/ludwig"
-    vim.g.omni_sql_no_default_maps = 1 -- https://www.reddit.com/r/vim/comments/2om1ib/how_to_disable_sql_dynamic_completion/cmop4zh/
-  end,
 }
