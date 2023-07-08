8c68e012860a5958a4dfa3473cba340cc5acdaa6
Jaye Heffernan <jpeheffernan@gmail.com>
2023-07-08 15:00:06 +1000

    Try fix formatting

--- home/.config/nvim/lua/plugins/null-ls.lua
+++ home/.config/nvim/lua/plugins/null-ls.lua
@@ -3,21 +3,44 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = { "mason.nvim" },
-    opts = function()
+    opts = function(_, opts)
       local nls = require("null-ls")
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          nls.builtins.formatting.fish_indent,
-          nls.builtins.diagnostics.fish,
-          nls.builtins.formatting.stylua,
-          nls.builtins.formatting.shfmt,
-          nls.builtins.diagnostics.sqlfluff,
-          -- Both formatters will run, in the order they're registered
-          nls.builtins.formatting.pg_format,
-          nls.builtins.formatting.sqlfluff,
-        },
+      -- -use this to extend instead
+      -- vim.list_extend(opts.sources, { ...
+      opts.sources = {
+        -- Prettierd is faster
+        nls.builtins.formatting.prettierd.with({
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "vue",
+            "css",
+            "scss",
+            "less",
+            "html",
+            "json",
+            "jsonc",
+            "yaml",
+          },
+          timeout = 8000,
+        }),
+        -- Prettierd seems to truncate my long markdown files often
+        nls.builtins.formatting.prettier.with({
+          filetypes = { "markdown", "markdown.mdx" },
+          timeout = 8000,
+        }),
+        nls.builtins.formatting.eslint_d.with({
+          timeout = 8000,
+        }),
+        nls.builtins.diagnostics.sqlfluff,
+        -- Both formatters will run, in the order they're registered
+        nls.builtins.formatting.pg_format,
+        nls.builtins.formatting.sqlfluff,
+        nls.builtins.code_actions.refactoring,
       }
+      return opts
     end,
   },
 }
