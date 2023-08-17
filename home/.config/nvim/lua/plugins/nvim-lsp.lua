7585fb66c9306bb2e4ede74b8daf1f4a6efe5bc9
Jaye Heffernan <jpeheffernan@gmail.com>
2023-08-17 15:03:12 +1000

    Fix autoformatting?

--- home/.config/nvim/lua/plugins/nvim-lsp.lua
+++ home/.config/nvim/lua/plugins/nvim-lsp.lua
@@ -3,8 +3,14 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
+      -- options for vim.lsp.buf.format
+      -- `bufnr` and `filter` is handled by the LazyVim formatter,
+      -- but can be also overridden when specified
       format = { timeout_ms = 6000 },
       autoformat = true,
+      autoformat = true,
+      -- Enable this to show formatters used in a notification
+      -- Useful for debugging formatter issues
       format_notify = true,
       ---@type lspconfig.options
       servers = {
@@ -38,8 +44,9 @@ return {
       setup = {
         efm = function(_, opts)
           require("lspconfig").efm.setup({
-            filetypes = { "javascript", "typescript" },
+            filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
             cmd = { "efm-langserver", "-c", "/Users/jaye.heffernan/.config/efm-langserver/config.yaml" },
+            init_options = { documentFormatting = true, documentRangeFormatting = true },
           })
           return true
         end,
