cbaef13d5a666148aa9ece72156efa57429b3a21
Jaye Heffernan <jpeheffernan@gmail.com>
2023-08-18 17:44:56 +1000

    Cleanup null-ls

--- home/.config/nvim/lua/plugins/disable.lua
+++ home/.config/nvim/lua/plugins/disable.lua
@@ -1,7 +1,8 @@
 return {
-  { "ggandor/flit.nvim",        enabled = false },
-  { "echasnovski/mini.animate", enabled = false },
+  { "ggandor/flit.nvim",               enabled = false },
+  { "echasnovski/mini.animate",        enabled = false },
   -- { "kdheepak/lazygit.nvim", enabled = false },
-  { "goolord/alpha-nvim",       enabled = false },
-  { "akinsho/bufferline.nvim",  enabled = false },
+  { "goolord/alpha-nvim",              enabled = false },
+  { "akinsho/bufferline.nvim",         enabled = false },
+  { "jose-elias-alvarez/null-ls.nvim", enabled = false },
 }
