306eb40b8988a0df379ae84fb0ccd4f95751f402
Jaye Heffernan <jpeheffernan@gmail.com>
2023-08-18 17:19:45 +1000

    Fix GBrowse to open GitHub

--- home/.config/nvim/lua/plugins/fugitive.lua
+++ home/.config/nvim/lua/plugins/fugitive.lua
@@ -2,4 +2,11 @@ return {
   {
     "tpope/vim-fugitive",
   },
+  {
+    "tpope/vim-rhubarb",
+  },
+  -- {
+  --   "iberianpig/tig-explorer.vim",
+  --   dependencies = { "mason.nvim" },
+  -- },
 }
