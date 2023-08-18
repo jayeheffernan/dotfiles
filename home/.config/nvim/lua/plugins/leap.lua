30cef68efcfe217de8b9837a2555ec70a832ab01
Jaye Heffernan <jpeheffernan@gmail.com>
2023-08-18 17:35:06 +1000

    Fix overridden surround mapping again

--- home/.config/nvim/lua/plugins/leap.lua
+++ home/.config/nvim/lua/plugins/leap.lua
@@ -21,33 +21,35 @@ return {
         },
       },
     },
-    keys = {
-      {
-        "s",
-        mode = { "n", "x", "o" },
-        function()
-          -- default options: exact mode, multi window, all directions, with a backdrop
-          require("flash").jump()
-        end,
-        desc = "Flash",
-      },
-      {
-        "S",
-        mode = { "n", "o" },
-        function()
-          require("flash").treesitter()
-        end,
-        desc = "Flash Treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function()
-          require("flash").remote()
-        end,
-        desc = "Remote Flash",
-      },
-    },
+    keys = function()
+      return {
+        {
+          "s",
+          mode = { "n", "x", "o" },
+          function()
+            -- default options: exact mode, multi window, all directions, with a backdrop
+            require("flash").jump()
+          end,
+          desc = "Flash",
+        },
+        {
+          "S",
+          mode = { "n", "o" },
+          function()
+            require("flash").treesitter()
+          end,
+          desc = "Flash Treesitter",
+        },
+        {
+          "r",
+          mode = "o",
+          function()
+            require("flash").remote()
+          end,
+          desc = "Remote Flash",
+        },
+      }
+    end,
   },
   -- ee13
   -- ee14
