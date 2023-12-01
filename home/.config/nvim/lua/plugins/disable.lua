830621b33279d23feeaea4fa30fe2471658dc9d1
Jaye Heffernan <jpeheffernan@gmail.com>
2023-12-01 21:42:58 +1100

    Update Vim plugins

--- home/.config/nvim/lua/plugins/disable.lua
+++ home/.config/nvim/lua/plugins/disable.lua
@@ -1,6 +1,6 @@
 return {
-  { "ggandor/flit.nvim",               enabled = false },
-  { "echasnovski/mini.animate",        enabled = false },
-  { "akinsho/bufferline.nvim",         enabled = false },
-  { "jose-elias-alvarez/null-ls.nvim", enabled = false },
+  { "ggandor/flit.nvim",        enabled = false },
+  { "echasnovski/mini.animate", enabled = false },
+  { "akinsho/bufferline.nvim",  enabled = false },
+  { "nvimtools/none-ls.nvim",   enabled = false },
 }
