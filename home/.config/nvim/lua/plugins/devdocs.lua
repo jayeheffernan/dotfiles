80b069e38509dce9d2c320a549270926ed529a8e
Jaye Heffernan <jpeheffernan@gmail.com>
2023-12-01 21:31:06 +1100

    More devdocs

--- home/.config/nvim/lua/plugins/devdocs.lua
+++ home/.config/nvim/lua/plugins/devdocs.lua
@@ -8,16 +8,29 @@ return {
     },
     opts = {
       ensure_installed = {
+        "bash",
+        "css",
+        "dom",
         "express",
+        "eslint", "prettier",
+        "svg",
         "git",
+        "http",
         "javascript",
+        "html", "jest", "typescript", "docker",
         "jsdoc",
         "lodash-4",
+        "lua-5.4",
         "moment",
         "moment_timezone",
         "node-18_lts",
         "postgresql-11",
         "react",
+        "redis",
+        "sass",
+        "web_extensions",
+        "webpack-4",
+        "webpack-5",
       }
     }
   }
