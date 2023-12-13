8ee8ea99c8838f9e3aa14a134492ff64709565bf
Jaye Heffernan <jpeheffernan@gmail.com>
2023-12-13 23:27:43 +1100

    Get Telescope working well??

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -10,84 +10,51 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
-    keys = function()
-      return {
-        -- { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
-        -- { "<leader>/", Util.telescope("live_grep"), desc = "Grep (root dir)" },
-        -- { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-        -- { "<leader><space>", Util.telescope("files"), desc = "Find Files (root dir)" },
-        -- find
-        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-        { "<leader>ff", Util.telescope("files"), desc = "Find Files (root dir)" },
-        { "<leader>fF", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
-        { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-        { "<leader>fR", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
-        -- git
-        { "<leader>sgc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
-        { "<leader>sgs", "<cmd>Telescope git_status<CR>", desc = "status" },
-        -- search
-        { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
-        { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
-        { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-        { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-        { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
-        { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace diagnostics" },
-        { "<leader>sf", Util.telescope("live_grep"), desc = "Find/Grep (root dir)" },
-        { "<leader>sF", Util.telescope("live_grep", { cwd = false }), desc = "Find/Grep (cwd)" },
-        { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
-        { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
-        { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Key Maps" },
-        { "<leader>sM", "<cmd>Telescope man_pages<cr>", desc = "Man Pages" },
-        { "<leader>sm", "<cmd>Telescope marks<cr>", desc = "Jump to Mark" },
-        { "<leader>so", "<cmd>Telescope vim_options<cr>", desc = "Options" },
-        { "<leader>sR", "<cmd>Telescope resume<cr>", desc = "Resume" },
-        { "<leader>sw", Util.telescope("grep_string"), desc = "Word (root dir)" },
-        { "<leader>sW", Util.telescope("grep_string", { cwd = false }), desc = "Word (cwd)" },
-        {
-          "<leader>uC",
-          Util.telescope("colorscheme", { enable_preview = true }),
-          desc = "Colorscheme with preview",
-        },
-        {
-          "<leader>ss",
-          Util.telescope("lsp_document_symbols", {
-            symbols = {
-              "Class",
-              "Function",
-              "Method",
-              "Constructor",
-              "Interface",
-              "Module",
-              "Struct",
-              "Trait",
-              "Field",
-              "Property",
-            },
-          }),
-          desc = "Goto Symbol",
-        },
-        {
-          "<leader>sS",
-          Util.telescope("lsp_dynamic_workspace_symbols", {
-            symbols = {
-              "Class",
-              "Function",
-              "Method",
-              "Constructor",
-              "Interface",
-              "Module",
-              "Struct",
-              "Trait",
-              "Field",
-              "Property",
-            },
-          }),
-          desc = "Goto Symbol (Workspace)",
-        },
-      }
-    end,
+    keys = {
+      -- Override search for "symbols"
+      { "<leader>ss", function() Util.telescope("grep_string", { use_regex = true, search = vim.fn.input("search") })() end, desc = "Grep (root dir)" },
+      {
+        "<leader>sS",
+        function()
+          Util.telescope("grep_string",
+            { cwd = false, use_regex = true, search = vim.fn.input("search") })()
+        end,
+        desc = "Grep (cwd)"
+      },
+      {
+        "<leader>sls",
+        function()
+          require("telescope.builtin").lsp_document_symbols({
+            symbols = require("lazyvim.config").get_kind_filter(),
+          })
+        end,
+        desc = "Goto LSP Symbol",
+      },
+      -- Restore these on different mappings
+      {
+        "<leader>slS",
+        function()
+          require("telescope.builtin").lsp_dynamic_workspace_symbols({
+            symbols = require("lazyvim.config").get_kind_filter(),
+          })
+        end,
+        desc = "Goto LSP Symbol (Workspace)",
+      },
+    },
     opts = {
       defaults = {
+        layout_strategy = "flex",
+        layout_config = {
+          flip_columns = 160,
+          height = { padding = 0 },
+          width = { padding = 0 },
+          horizontal = {
+            preview_width = 0.35,
+          },
+          vertical = {
+            preview_height = 0.3,
+          },
+        },
         prompt_prefix = " ",
         selection_caret = " ",
         mappings = {
