0d3a50f542df00b9d90971a0832463e6a9821735
Jaye Heffernan <jpeheffernan@gmail.com>
2023-12-13 23:40:26 +1100

    Toggle Telescope preview

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -65,28 +65,31 @@ return {
               actions.open_qflist(...)
               vim.cmd("cc") -- show first quickfix error
             end,
-            ["<c-t>"] = function(...)
-              return require("trouble.providers.telescope").open_with_trouble(...)
+            ["<c-h>"] = function(...)
+              return require('telescope.actions.layout').toggle_preview(...)
             end,
-            ["<a-t>"] = function(...)
-              return require("trouble.providers.telescope").open_selected_with_trouble(...)
-            end,
-            ["<a-i>"] = function()
-              local action_state = require("telescope.actions.state")
-              local line = action_state.get_current_line()
-              Util.telescope("find_files", { no_ignore = true, default_text = line })()
-            end,
-            ["<a-h>"] = function()
-              local action_state = require("telescope.actions.state")
-              local line = action_state.get_current_line()
-              Util.telescope("find_files", { hidden = true, default_text = line })()
-            end,
-            ["<C-Down>"] = function(...)
-              return require("telescope.actions").cycle_history_next(...)
-            end,
-            ["<C-Up>"] = function(...)
-              return require("telescope.actions").cycle_history_prev(...)
-            end,
+            -- ["<c-t>"] = function(...)
+            --   return require("trouble.providers.telescope").open_with_trouble(...)
+            -- end,
+            -- ["<a-t>"] = function(...)
+            --   return require("trouble.providers.telescope").open_selected_with_trouble(...)
+            -- end,
+            -- ["<a-i>"] = function()
+            --   local action_state = require("telescope.actions.state")
+            --   local line = action_state.get_current_line()
+            --   Util.telescope("find_files", { no_ignore = true, default_text = line })()
+            -- end,
+            -- ["<a-h>"] = function()
+            --   local action_state = require("telescope.actions.state")
+            --   local line = action_state.get_current_line()
+            --   Util.telescope("find_files", { hidden = true, default_text = line })()
+            -- end,
+            -- ["<C-Down>"] = function(...)
+            --   return require("telescope.actions").cycle_history_next(...)
+            -- end,
+            -- ["<C-Up>"] = function(...)
+            --   return require("telescope.actions").cycle_history_prev(...)
+            -- end,
             ["<C-f>"] = function(...)
               return require("telescope.actions").preview_scrolling_down(...)
             end,
@@ -96,14 +99,11 @@ return {
             ["<C-c>"] = function(...)
               return require("telescope.actions").close(...)
             end,
-            ["Esc"] = function(...)
-              return require("telescope.actions").close(...)
-            end,
           },
           n = {
-            ["q"] = function(...)
-              return require("telescope.actions").close(...)
-            end,
+            -- ["q"] = function(...)
+            --   return require("telescope.actions").close(...)
+            -- end,
           },
         },
       },
