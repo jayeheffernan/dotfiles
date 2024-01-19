27292670926bc19f4a8aaea0c12d0b757b9ea5a6
Jaye Heffernan <jpeheffernan@gmail.com>
2024-01-19 18:12:57 +1100

    Fix voice input for ChatGPT

--- home/.config/nvim/lua/plugins/chatgpt.lua
+++ home/.config/nvim/lua/plugins/chatgpt.lua
@@ -2,7 +2,10 @@ return {
   {
     "robitx/gp.nvim",
     config = function()
-      require("gp").setup()
+      require("gp").setup({
+        whisper_dir = "/tmp/gp_whisper",
+        whisper_rec_cmd = "sox",
+      })
     end,
   }
 }
